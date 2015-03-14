68316d5a6c175930a458a491623b43198edb701c
mamoon1428 <ee113140@gmail.com>
2015-03-15 01:30:39 +0500

    testing...

--- TricorERP/Tricor.Master.designer.cs
+++ TricorERP/Tricor.Master.designer.cs
@@ -139,6 +139,15 @@ namespace TricorERP {
         protected global::System.Web.UI.WebControls.HyperLink HyperLink11;
         
         /// <summary>
+        /// HyperLink15 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink15;
+        
+        /// <summary>
         /// HyperLink12 control.
         /// </summary>
         /// <remarks>
