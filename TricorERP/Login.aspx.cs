22f9efb36fc6ec77bba5339c68846c0119416017
hasnatali51 <alihasnat1892@gmail.com>
2015-03-16 14:43:54 +0500

    asd

--- TricorERP/Login.aspx.cs
+++ TricorERP/Login.aspx.cs
@@ -18,24 +18,10 @@ namespace TricorERP.POS
 
         protected void Login_Click(object sender, EventArgs e)
         {
-            //String gender = "";
-            //bool ischeck = BranchManagerRadio.Checked;
-            //if (ischeck)
-            //    gender = BranchManagerRadio.Text;
-            //else
-            //    gender = CashierRadio.Text;
-
-            //Q. how can we use model class?
-            //LoginModel user = new LoginModel();
-            //user.userName = NameTextBox.Text;
-            //user.password = PasswordTextBox.Text;
-            //user.userType = gender; // set value in integer
-            
             UserModel userModel = UserLogin.loginCheck(NameTextBox.Text, PasswordTextBox.Text); // static method
 
             if (userModel != null)
             {
-                loginMsg.Text = "<h4>You Choose Branch Manager OR Cashier </h4>";
                 Session["Username"] = NameTextBox.Text;
                 Session["RoleID"] = userModel.RoleID;
                 Response.Redirect("~/Home.aspx");
