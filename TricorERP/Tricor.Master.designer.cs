3c6b57738537b1d0fc9045748168974c20133a1d
hasnatali51 <alihasnat1892@gmail.com>
2015-03-16 22:42:22 +0500

    changing n master page

--- TricorERP/Tricor.Master.designer.cs
+++ TricorERP/Tricor.Master.designer.cs
@@ -274,24 +274,6 @@ namespace TricorERP {
         protected global::System.Web.UI.WebControls.HyperLink HyperLink19;
         
         /// <summary>
-        /// SaleorderLink control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton SaleorderLink;
-        
-        /// <summary>
-        /// LinkButton2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton LinkButton2;
-        
-        /// <summary>
         /// LinkButton1 control.
         /// </summary>
         /// <remarks>
