f2ca36ac19e23927d66412ca1c09fa77ade94843
hasnatali51 <alihasnat1892@gmail.com>
2015-03-20 02:43:04 +0500

    new pages addition

--- TricorERP/POS/Order/NewOrder.aspx.cs
+++ TricorERP/POS/Order/NewOrder.aspx.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using Models.POS.Customer;
 
 namespace TricorERP.POS.Order
 {
@@ -14,29 +13,5 @@ namespace TricorERP.POS.Order
         {
 
         }
-
-        protected void CustomerDropDownList1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            //List <CustomerModel> allcustomer = null;
-            //allcustomer = Database.POS.Customer.CustomerDB.getallCustomer();
-            //CustomerDropDown.DataBind = allcustomer;
-            //CustomerDropDown.DataBind();
-        }
-
-        protected void AddProducts_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Savebtn_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void btnCancel_Click(object sender, EventArgs e)
-        {
-
-        }
-
     }
 }
