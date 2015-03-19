77e936f7a109434c6a68052babbff7a4797004ed
mamoon1428 <ee113140@gmail.com>
2015-03-20 01:43:32 +0300

    new changes in newpages

---
+++ Models/SCM/WHModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.SCM
+{
+    public class WHModel
+    {
+            public int ID { set; get; }
+            public String Name { set; get; }
+            public String Location1 { set; get; }
+            public String Location2 { set; get; }
+
+            public String Phonenumber { set; get; }
+            public String City { set; get; }
+            public String Email { set; get; }
+        }
+    }
+}
