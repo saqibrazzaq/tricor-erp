c006c0f469cc7e2d000c628b09761175592d878c
hasnatali51 <alihasnat1892@gmail.com>
2015-03-20 13:19:40 +0500

    new

--- TricorERP/POS/Order/CreateOrder.aspx.cs
+++ TricorERP/POS/Order/CreateOrder.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace TricorERP.POS.Order
 {
-    public partial class Order : System.Web.UI.Page
+    public partial class CreateOrder : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
